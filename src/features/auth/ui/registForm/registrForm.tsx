9d6fcb21270da853af682e0b8ba526430726c4e1
Bully_Thunder <vamosbarca2407@gmail.com>
2026-03-31 11:32:46 +0300

    feat (auth) : create validation on  form

--- src/features/auth/ui/registForm/registrForm.tsx
+++ src/features/auth/ui/registForm/registrForm.tsx
@@ -6,28 +6,47 @@ import { Icons } from '@/shared/Icon';
 import { Input } from '@/shared/ui/Input';
 import { Button } from '@/shared/ui/Button';
 import { registrFormProps } from '@/features/auth/ui/registForm/types';
+import { registerUser } from '@/services/auth/auth';
+
 import { useState } from 'react';
 
 export const RegistrForm = ({ onClose }: registrFormProps) => {
   const [isEmail, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+  const [isNames, setName] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [completeMessage, setCompleteMessage] = useState('');
+  const [errorPasswordMessage, setErrorPasswordMessage] = useState('');
+  const [isPassword, setPassword] = useState('');
+  const [isConfirmPassword, setConfirmPassword] = useState('');
+
+  const handleLoginRegistration = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log('From sended');
-  };
-  const handleOverlayClick = (e: React.MouseEvent) => {
-    // e.target — это то, на что физически нажали
-    // e.currentTarget — это элемент, на котором висит этот обработчик (фон)
-    if (e.target === e.currentTarget) {
-      onClose(); // Закрываем только если кликнули именно по фону
+    setErrorMessage('');
+    if (isPassword !== isConfirmPassword) {
+      setErrorMessage('passwrod don`t match');
+      return;
+    }
+    try {
+      const sendedData = {
+        email: isEmail,
+        password: isPassword,
+        name: isNames,
+      };
+      await registerUser(sendedData);
+      console.log('Data is send');
+      setCompleteMessage('Registration is complete');
+      onClose();
+    } catch (error) {
+      setErrorMessage('Error registration');
+      console.log(error);
     }
   };
-
   return (
     <div className="font-fixelDisplay flex w-full max-w-sm flex-col items-center justify-center rounded-[24px] bg-[url('/avltu/bg-form.png')] p-10">
-      <form onSubmit={handleLogin} className="flex flex-col items-center justify-center gap-[60px]">
+      <form
+        onSubmit={handleLoginRegistration}
+        className="flex flex-col items-center justify-center gap-[60px]"
+      >
         <div className="flex items-end justify-center gap-[10px]">
           <h2 className="font-dihjauti text-[48px] leading-none text-white">Registration</h2>
           <img src="/avltu/logo_alvtu.svg" alt="logo" className="h-auto w-[40px]" />
@@ -35,7 +54,7 @@ export const RegistrForm = ({ onClose }: registrFormProps) => {
         <div className="flex flex-col gap-[40px]">
           <Input
             type="name"
-            value={name}
+            value={isNames}
             onChange={(e) => setName(e.target.value)}
             placeholder="Введіть ваше ім'я"
           />
@@ -48,16 +67,20 @@ export const RegistrForm = ({ onClose }: registrFormProps) => {
           <Input
             type="password"
             placeholder="Введіть пароль"
-            value={password}
+            value={isPassword}
             onChange={(e) => setPassword(e.target.value)}
           />
           <Input
             type="password"
             placeholder="Повторіть пароль"
-            value={confirmPassword}
+            value={isConfirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
-          <Button className="cursor-pointer">Зарееструватися</Button>
+          {completeMessage && <p style={{ color: 'green', fontSize: '14px' }}>{errorMessage}</p>}
+          {errorMessage && <p style={{ color: 'red', fontSize: '14px' }}>{errorMessage}</p>}
+          <Button type="submit" className="cursor-pointer">
+            Зарееструватися
+          </Button>
         </div>
       </form>
     </div>
